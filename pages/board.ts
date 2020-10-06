075258788a14415bd5db846d9ecbff117b3ea819
Dashup <git@dashup.io>
2020-10-06 16:54:26 +1000

    add filtering

--- pages/board.ts
+++ pages/board.ts
@@ -48,6 +48,7 @@ export default class BoardPage extends Struct {
       view   : 'page/board/view',
       menu   : 'page/board/menu',
       config : 'page/board/config',
+      filter : 'page/board/filter',
     };
   }
 
